bee61a98475899cb008c75abb55b29a7e8aaa1b0
Duncan McPherson <d.mcpherson.home@gmail.com>
2026-07-07 15:17:57 -0600

    feat: Add validator methods for finding the repo root
    - Add method for locating repo root from a specified start path
    - Add method that throws an error if current directory is not inside a repo

---
+++ gud/Utilities/GudRepository.cs
@@ -0,0 +1,26 @@
+namespace gud.Utilities;
+
+public static class GudRepository
+{
+    public static string? FindRoot(string startPath)
+    {
+        var dir = new DirectoryInfo(startPath);
+        while (dir != null)
+        {
+            if (Directory.Exists(Path.Combine(dir.FullName, ".gud")))
+            {
+                return dir.FullName;
+            }
+
+            dir = dir.Parent;
+        }
+
+        return null;
+    }
+
+    public static string? RequireRoot()
+    {
+        var root = FindRoot(Directory.GetCurrentDirectory());
+        return root ?? throw new InvalidOperationException("Not a gud repository");
+    }
+}
