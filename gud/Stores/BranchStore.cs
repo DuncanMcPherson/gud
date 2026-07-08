c1caf5f57f39a5f51cb958ff9e72a1aa28847e73
Duncan McPherson <d.mcpherson.home@gmail.com>
2026-07-08 07:25:49 -0600

    created initial branch store

---
+++ gud/Stores/BranchStore.cs
@@ -0,0 +1,25 @@
+namespace gud.Stores;
+
+public class BranchStore(string gudPath)
+{
+    private readonly string _headsPath = Path.Join(gudPath, "refs", "heads");
+
+    public IEnumerable<string?> ListBranches()
+    {
+        return Directory.Exists(_headsPath) ? Directory.GetFiles(_headsPath).Select(Path.GetFileName) : Enumerable.Empty<string>();
+    }
+
+    public bool Exists(string branch) => File.Exists(Path.Join(_headsPath, branch));
+
+    public string? GetCommit(string name)
+    {
+        var path = Path.Join(_headsPath, name);
+        return File.Exists(path) ? File.ReadAllText(path).Trim() : null;
+    }
+
+    public void SetCommit(string branch, string commit)
+    {
+        var path = Path.Join(_headsPath, branch);
+        File.WriteAllText(path, commit);
+    }
+}
