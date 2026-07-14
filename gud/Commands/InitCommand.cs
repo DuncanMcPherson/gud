595c1ae2bf6cce2ab2671f7c2322bdea831d30e0
Duncan McPherson <d.mcpherson.home@gmail.com>
2026-07-14 12:52:52 -0600

    fix test

--- gud/Commands/InitCommand.cs
+++ gud/Commands/InitCommand.cs
@@ -7,7 +7,7 @@ namespace gud.Commands;
 public class InitCommand : AsyncCommand<InitCommand.Settings>
 {
     private readonly IAnsiConsole _console;
-    
+
     public InitCommand(IAnsiConsole console)
     {
         _console = console;
@@ -17,11 +17,20 @@ public class InitCommand : AsyncCommand<InitCommand.Settings>
     {
     }
 
-    protected override async Task<int> ExecuteAsync(CommandContext context, Settings settings, CancellationToken cancellationToken)
+    protected override async Task<int> ExecuteAsync(CommandContext context, Settings settings,
+        CancellationToken cancellationToken)
     {
-        var gudPath = await GudRepository.CreateAsync(Directory.GetCurrentDirectory());
-        
-        _console.MarkupLine($"[green]Initialized empty gud repository[/] in {gudPath}");
-        return 0;
+        try
+        {
+            var gudPath = await GudRepository.CreateAsync(Directory.GetCurrentDirectory());
+
+            _console.MarkupLine($"[green]Initialized empty gud repository[/] in {gudPath}");
+            return 0;
+        }
+        catch (InvalidOperationException ex)
+        {
+            _console.MarkupLine($"[red]Error: {ex.Message}[/]");
+            return 1;
+        }
     }
 }
