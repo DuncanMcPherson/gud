8a35a678bb6d7cf9df0c8fc2b7aaba59958f507c
J Duncan McPherson <139790207+DuncanMcPherson@users.noreply.github.com>
2026-07-14 12:53:43 -0600

    feat: Consolidate repo creation logic (#63)
    - Added CreateAsync to GudRepository to keep init logic easy to maintain
    - Added GET /repos to list repositories. This will eventually require auth
    - Added POST /repos/{repo} for creating a repo

--- gud/Commands/InitCommand.cs
+++ gud/Commands/InitCommand.cs
@@ -1,4 +1,5 @@
-﻿using Spectre.Console;
+﻿using gud.Core.Utilities;
+using Spectre.Console;
 using Spectre.Console.Cli;
 
 namespace gud.Commands;
@@ -6,7 +7,7 @@ namespace gud.Commands;
 public class InitCommand : AsyncCommand<InitCommand.Settings>
 {
     private readonly IAnsiConsole _console;
-    
+
     public InitCommand(IAnsiConsole console)
     {
         _console = console;
@@ -16,22 +17,20 @@ public class InitCommand : AsyncCommand<InitCommand.Settings>
     {
     }
 
-    protected override async Task<int> ExecuteAsync(CommandContext context, Settings settings, CancellationToken cancellationToken)
+    protected override async Task<int> ExecuteAsync(CommandContext context, Settings settings,
+        CancellationToken cancellationToken)
     {
-        var gudPath = Path.Combine(Directory.GetCurrentDirectory(), ".gud");
-
-        if (Directory.Exists(gudPath))
+        try
         {
-            _console.MarkupLine("[red]Error:[/] Repository already initialized here");
+            var gudPath = await GudRepository.CreateAsync(Directory.GetCurrentDirectory());
+
+            _console.MarkupLine($"[green]Initialized empty gud repository[/] in {gudPath}");
+            return 0;
+        }
+        catch (InvalidOperationException ex)
+        {
+            _console.MarkupLine($"[red]Error: {ex.Message}[/]");
             return 1;
         }
-
-        Directory.CreateDirectory(Path.Combine(gudPath, "objects"));
-        Directory.CreateDirectory(Path.Combine(gudPath, "refs", "heads"));
-
-        await File.WriteAllTextAsync(Path.Combine(gudPath, "HEAD"), "ref: refs/heads/main\n", cancellationToken);
-        
-        _console.MarkupLine($"[green]Initialized empty gud repository[/] in {gudPath}");
-        return 0;
     }
 }
