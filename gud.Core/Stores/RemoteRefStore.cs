6679423f55b6caea12e67d58bc6c9cfd5c9c90b0
Duncan McPherson <d.mcpherson.home@gmail.com>
2026-07-20 14:59:58 -0600

    start of fetch work

--- gud.Core/Stores/RemoteRefStore.cs
+++ gud.Core/Stores/RemoteRefStore.cs
@@ -1,4 +1,8 @@
-﻿namespace gud.Core.Stores;
+﻿using gud.Core.Models;
+using gud.Core.Repository;
+using gud.Core.Services;
+
+namespace gud.Core.Stores;
 
 public class RemoteRefStore(string gudPath)
 {
@@ -15,5 +19,57 @@ public class RemoteRefStore(string gudPath)
         var path = Path.Combine(_remoteRefsPath, remote, branch.Replace('/', Path.DirectorySeparatorChar));
         Directory.CreateDirectory(Path.GetDirectoryName(path)!);
         File.WriteAllText(path, commit);
+    }
+
+    public async Task<int> FetchReachable(
+        GudRemoteClient client,
+        ObjectStore objectStore,
+        ObjectRepository objects,
+        string startCommit)
+    {
+        var visited = new HashSet<string>();
+        var queue = new Queue<(string hash, ObjectType type)>();
+        queue.Enqueue((startCommit, ObjectType.Commit));
+
+        var fetchedCount = 0;
+
+        while (queue.Count > 0)
+        {
+            var (hash, _) = queue.Dequeue();
+            if (!visited.Add(hash)) continue;
+
+            byte[] rawContent;
+            if (objectStore.Exists(hash))
+            {
+                rawContent = objects.ReadRawObjectFile(hash);
+            }
+            else
+            {
+                rawContent = await client.GetObjectAsync(hash);
+                objectStore.Write(hash, rawContent);
+                fetchedCount++;
+            }
+
+            var (type, content) = ObjectRepository.ParseRaw(rawContent);
+            switch (type)
+            {
+                case ObjectType.Commit:
+                    var commit = Commit.Read(content);
+                    queue.Enqueue((commit.TreeHash, ObjectType.Tree));
+                    foreach (var parent in commit.ParentHashes)
+                        queue.Enqueue((parent, ObjectType.Commit));
+                    break;
+                case ObjectType.Tree:
+                    var tree = Tree.Read(content);
+                    foreach (var entry in tree.Entries)
+                        queue.Enqueue((entry.Hash, entry.Type == TreeEntryType.Blob ? ObjectType.Blob : ObjectType.Tree));
+                    break;
+                case ObjectType.Blob:
+                    // Leaf node, nothing to do
+                    break;
+            }
+        }
+
+        return fetchedCount;
     }
 }
